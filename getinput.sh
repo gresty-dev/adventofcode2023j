9ac2c017777f9cac07e50f30dc4ca10cb38514c5
Chris Gresty <chris@gresty.dev>
2023-12-09 21:38:38 +0000

    User agent header and Readme, to support AoC automation guidelines.

--- getinput.sh
+++ getinput.sh
@@ -2,5 +2,10 @@
 DAY=$1
 FORMATTED_DAY=`printf "%02d" $1`
 OUTPUT="src/main/resources/dev/gresty/aoc2023/$FORMATTED_DAY"
+if [ -f $OUTPUT ]; then
+  echo "Puzzle input file for day $FORMATTED_DAY already exists."
+  exit
+fi
 INPUT="https://adventofcode.com/2023/day/$DAY/input"
-doppler run --command="curl -o $OUTPUT --cookie session=\$AOC_SESSION $INPUT"
+USER_AGENT='github.com/gresty-dev/adventofcode2023j - contact chris@gresty.dev'
+doppler run --command="curl -v -A \"$USER_AGENT\" -o $OUTPUT --cookie session=\$AOC_SESSION $INPUT"
