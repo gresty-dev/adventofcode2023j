ae8c3a426d030849a464c74258fd72968c20a190
Chris Gresty <chris.gresty@anaplan.com>
2023-12-12 15:42:56 +0000

    Day 12 base

---
+++ src/main/java/dev/gresty/aoc2023/Day12.java
@@ -0,0 +1,23 @@
+package dev.gresty.aoc2023;
+
+public class Day12 implements IPuzzle {
+
+    public static void main(String[] args) {
+        Main.execute(12);
+    }
+
+    private final PuzzleInput input;
+
+    public Day12(final PuzzleInput input) {
+        this.input = input;
+    }
+
+    public String part1() {
+        return "Not implemented";
+    }
+
+    public String part2() {
+        return "Not implemented";
+    }
+
+}
---
+++ src/test/java/dev/gresty/aoc2023/Day12Test.java
@@ -0,0 +1,26 @@
+package dev.gresty.aoc2023;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Day12Test {
+
+    private static final String EXAMPLE = """
+            ???.### 1,1,3
+            .??..??...?##. 1,1,3
+            ?#?#?#?#?#?#?#? 1,3,1,6
+            ????.#...#... 4,1,1
+            ????.######..#####. 1,6,5
+            ?###???????? 3,2,1""";
+
+    @Test
+    public void testPart1() {
+        assertThat(new Day12(PuzzleInput.of(EXAMPLE)).part1()).isEqualTo("21");
+    }
+
+    @Test
+    public void testPart2() {
+        assertThat(new Day12(PuzzleInput.of(EXAMPLE)).part2()).isEqualTo("Not implemented");
+    }
+}
